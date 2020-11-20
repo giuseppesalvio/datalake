980455000ea0be0f6a85a96574cfdcff75557917
g.salvio <giuseppe.salvio@finconsgroup.com>
2020-11-20 18:21:15 +0100

    fix, e query

--- src/main/java/com/fincons/datalake/controller/AllController.java
+++ src/main/java/com/fincons/datalake/controller/AllController.java
@@ -5,6 +5,7 @@ import com.fincons.datalake.service.InserimentoDanni;
 import com.fincons.datalake.service.InserimentoVita;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,16 +21,18 @@ public class AllController {
     private InserimentoCommander inserimentoCommander;
 
     @GetMapping("/riempiDb")
-    public void riempiDb(){
+    public void riempiDb(@RequestParam int start, @RequestParam int end){
         // FISICA
-        for (int i = START_FOR_PF; i < END_FOR_PF; i++) {
+        for (int i = start; i < end; i++) {
             Integer ecidContraente = inserimentoCommander.pf();
             inserimentoVita.pf(ecidContraente);
             inserimentoDanni.pf(ecidContraente);
         }
 
         // GIURIDICA
-        for (int i = START_FOR_PG; i < END_FOR_PG; i++) {
+        int startG = end + 1;
+        int endG = end + end;
+        for (int i = startG; i < endG; i++) {
             Integer ecidContraente = inserimentoCommander.pg();
             inserimentoVita.pg(ecidContraente);
             inserimentoDanni.pg(ecidContraente);
